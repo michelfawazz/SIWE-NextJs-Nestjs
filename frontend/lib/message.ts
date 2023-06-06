dc2db2d3ec1780c98a1bde13050c8cfa8cd82b63
Michel Fawaz <Michelfawaz87@gmail.com>
2023-06-06 16:30:23 +0300

    env

--- frontend/lib/message.ts
+++ frontend/lib/message.ts
@@ -10,7 +10,7 @@ export async function verifySiwe(address: string) {
     )
 
     const messageToSign = new SiweMessage({
-        domain: process.env.NEXT_PUBLIC_DOMAIN ||"localhost:3000",
+        domain: process.env.NEXT_PUBLIC_DOMAIN,
         address: address,
         statement: "Sign in with Ethereum to the app",
         uri: window.location.origin,
